c9e0968fab7578002a12dba8684192f11dc747e9
pipehat <>
2023-08-21 07:08:16 -0400

    added forward motion so you can actually fly around

--- plane.gd
+++ plane.gd
@@ -2,12 +2,14 @@ extends CharacterBody3D
 
 var print_delay = 1
 var next_print = 0
+const SPEED_MPS = 500
 
 func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
 	if next_print <= 0:
 		print(input_dir)
+		print("-z: ", -transform.basis.z)
 		next_print = print_delay
 	else:
 		next_print -= delta
@@ -17,3 +19,6 @@ func _physics_process(delta):
 	
 	rotate(transform.basis.z, roll * delta)
 	rotate(transform.basis.x, pitch  * delta)
+
+	velocity = -transform.basis.z * SPEED_MPS * delta
+	move_and_slide()
