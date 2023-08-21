0c39eddb49965c1ac8baca9e355c91ecf46b76bc
pipehat <>
2023-08-21 08:14:54 -0400

    Add a little world-y axis rotation based on plane's z-rotation.
    So banking left or right results in the nose drifting in that direction.

--- plane.gd
+++ plane.gd
@@ -7,18 +7,33 @@ const SPEED_MPS = 500
 func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
+	var roll = -input_dir[0]
+	var pitch = input_dir[1]
+	var forward = -transform.basis.z
+	
+	var z_axis_rotation = rotation.z # -PI..PI
+	var z_rotation_normalized = z_axis_rotation / PI # -1..1
+	if z_rotation_normalized > 0.5:
+		z_rotation_normalized = 1 - z_rotation_normalized
+	elif z_rotation_normalized < -0.5:
+		z_rotation_normalized = -1 - z_rotation_normalized
+	
+	# give a little yaw rotation based on current z rotation
+	var yaw_drift = z_rotation_normalized * PI * .33 * delta
+	
 	if next_print <= 0:
-		print(input_dir)
-		print("-z: ", -transform.basis.z)
+		print("roll ", roll,
+			  "   pitch ", pitch,
+			  "   forward: ", forward)
+		print("rotation: ", rotation)
+		print("z_rotation_normalized: ", z_rotation_normalized)
 		next_print = print_delay
 	else:
 		next_print -= delta
-	
-	var roll = -input_dir[0]
-	var pitch = input_dir[1]
-	
+
 	rotate(transform.basis.z, roll * delta)
 	rotate(transform.basis.x, pitch  * delta)
+	rotate_y(yaw_drift)
 
 	velocity = -transform.basis.z * SPEED_MPS * delta
 	move_and_slide()
